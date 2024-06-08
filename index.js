dac1595d40425ce3230d91a32f0011391fbc95d2
hexaredecimal <mfanakagama@gmail.com>
2024-06-08 15:32:58 +0200

    index: Adds functions for getting data from the database

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+const addBtn = (id) => {
+  document.getElementById(`open_${id}`).addEventListener('click', () => {
+    document.getElementById(`${id}_dialog`).open = true;
+  });
+  document.getElementById(`close_${id}`).addEventListener('click', () => {
+    document.getElementById(`${id}_dialog`).open = false;
+  });
+}
+
+const closeDialog = (id) => {
+    document.getElementById(`${id}_dialog`).open = false;
+}
+
+const loadPage = () => {
+  addBtn('login')
+  addBtn('submit')
+}
+
+
